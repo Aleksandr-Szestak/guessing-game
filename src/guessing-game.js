f133fa1e5c7b6620062c24240ea27138e690a756
Aleksandr-Szestak <szestak@hotmail.com>
2023-02-21 13:15:37 +0300

    init: first version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.fromVal = 0;
+        this.toVal   = 0;
+        this.lastVal = 0;
+        this.counter = 0;
+    }
 
     setRange(min, max) {
-
+        this.fromVal = min;
+        this.toVal = max;
     }
 
     guess() {
+        let amountNumbers = this.toVal - this.fromVal;
+        let averageVal = this.fromVal + Math.ceil(amountNumbers / 2);
 
+        this.counter++;
+        this.lastVal = averageVal;
+        //console.log(this.counter, averageVal);
+
+        return averageVal;
     }
 
     lower() {
-
+        this.toVal = this.lastVal;
     }
 
     greater() {
-
+        this.fromVal = this.lastVal;
     }
 }
 
