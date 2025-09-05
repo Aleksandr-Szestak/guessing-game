12410a722ae03592af5413d5dafae598420146df
Aleksandr-Szestak <szestak@hotmail.com>
2025-09-05 13:50:53 +0300

    feat: add implementation of task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.fromVal = 0;
+    this.toVal = 0;
+    this.lastVal = 0;
+    this.counter = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.fromVal = min;
+    this.toVal = max;
+  }
 
-  lower() { }
+  guess() {
+    const amountNumbers = this.toVal - this.fromVal;
+    const averageVal = this.fromVal + Math.ceil(amountNumbers / 2);
 
-  greater() { }
-*/
+    this.counter += 1;
+    this.lastVal = averageVal;
+
+    return averageVal;
+  }
+
+  lower() {
+    this.toVal = this.lastVal;
+  }
+
+  greater() {
+    this.fromVal = this.lastVal;
+  }
 }
 
 module.exports = GuessingGame;
